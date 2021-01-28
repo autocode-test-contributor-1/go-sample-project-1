2cf01a9dd177ed2092afb62d91c7599ec40b8af3
autocode-test-user-2 <autocode.test.user2@mail.ru>
2021-01-28 12:28:05 +0300

    added quality gate sample

--- calculator.go
+++ calculator.go
@@ -10,8 +10,11 @@ func multiply(x int, y int) int {
 }
 
 func subtraction(x int, y int) int {
-	var c = 10 //useless variable and commented out code
-	return x - y
+	var foo int
+	if true {
+		foo = y
+	}
+	return x - foo
 }
 
 func divide(x int, y int) int {
