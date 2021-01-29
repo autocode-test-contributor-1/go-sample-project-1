57c15e53dd5434866a53f4b902f3a6873af06981
autocode-test-user-2 <70654538+autocode-test-user-2@users.noreply.github.com>
2021-01-29 11:42:46 +0300

    Update calculator.go

--- calculator.go
+++ calculator.go
@@ -1,7 +1,24 @@
 package calculator
 
 func add(x int, y int) int {
+	//self-assigned variable
+	x = x
+
+	//duplicated string literals
+	var a string = "Hello"
+	var b string = "Hello"
+	var c string = "Hello"
+	if a == "Hello" && b == "Hello" && c == "Hello" {
 	return x + y
+	} else if a == "Hello" && b == "Hello" && c == "Hello" { //same condition
+	return x + y
+	} else {
+	return 1
+	}
+}
+
+//empty function
+func doNothing() {
 }
 
 func multiply(x int, y int) int {
@@ -9,9 +26,12 @@ func multiply(x int, y int) int {
 }
 
 func subtraction(x int, y int) int {
-	return x - y
+	return x - y + 2
 }
 
 func divide(x int, y int) int {
+	if true {
+	y = 0
+	}
 	return x / y
 }
